cb4c7062a18c8e9a1b86ac275ca139ab1bf4300e
LongamRangnamei <165648037+LongamRangnamei@users.noreply.github.com>
2024-07-20 23:01:05 +0530

    Add files via upload

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4200',
-    'https://ubiquitous-nasturtium-adaa4a.netlify.app' ,'https://ubiquitous-nasturtium-adaa4a.netlify.app/'
+    'https://ubiquitous-nasturtium-adaa4a.netlify.app'
 
 ];
 
